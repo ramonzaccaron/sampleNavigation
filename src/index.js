a10bc5ae7525544f2076c049f156f435b3e2b359
ramonzaccaron <ramonzaccaron@hotmail.com>
2019-05-21 22:28:40 -0300

    Implementado navegação via createDrawerNavigator

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import Page1 from './Page1';
 import Page2 from './Page2';
 import Page3 from './Page3';
 
-import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import { createDrawerNavigator, createAppContainer } from "react-navigation";
 
-const Routes = createBottomTabNavigator({
+const Routes = createDrawerNavigator({
     Home: { screen: Page1 },
     About: { screen: Page2 },
     Help: {screen: Page3}
