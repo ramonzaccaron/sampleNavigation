e707a0ec7eb52f3e9c94e0a22a589d8ffb308bff
ramonzaccaron <ramonzaccaron@hotmail.com>
2019-05-21 22:33:16 -0300

    mantido padrão TabNavigator

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import Page1 from './Page1';
 import Page2 from './Page2';
 import Page3 from './Page3';
 
-import { createDrawerNavigator, createAppContainer } from "react-navigation";
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 
-const Routes = createDrawerNavigator({
+//createStackNavigator 
+//createDrawerNavigator + Main (android)
+//createBottomTabNavigator + react-native-vector-icons
+const Routes = createBottomTabNavigator({
     Home: { screen: Page1 },
     About: { screen: Page2 },
     Help: {screen: Page3}
